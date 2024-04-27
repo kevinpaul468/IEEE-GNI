5cd61242f4e06e2e11269095e25173f694b853b9
Singireddy Sumith reddy <sumithreddy.6080@gmail.com>
2024-04-27 23:07:13 +0530

    change nav

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -10,47 +10,32 @@ import {
 
 import CardItem from "@/components/Items/CardItem";
 
-
 function Events() {
   return (
     <div id="events">
       <div>
-        <div className="  h-screen eventsbg border-b-8 border-red-500  w-full">
+        <div className="   eventsbg border-b-8 border-red-500  w-full">
           <p>.</p>
           <div className="heading  txtcolor  text-5xl  ">
-            <p className=" ml-[25px] mt-12 text-white">EVENTS</p>
+            <p className=" ml-[25px] mt-10 text-center text-white">EVENTS</p>
           </div>
-          <div className="cerodrag m-6">
-            {/* <CarouselContent>
-                <CarouselItem className="md:basis-1/2  pl-16 lg:basis-2/3">
-                  <CardItem />
-                </CarouselItem>
-               
-                <CarouselItem className="md:basis-1/2  pl-16 lg:basis-2/3">
-                  <CardItem />
-                </CarouselItem>
-                <CarouselItem className="md:basis-1/2 pl-16  lg:basis-2/3">
-                  <CardItem />
-                </CarouselItem>
-              </CarouselContent> */}
-                 <Carousel>
-                 <CarouselContent> 
-            {EventInfo.map((eve) => {
-              return (
-                <div key={eve.id}>
-               
-                    <CarouselItem className="md:basis-1/2  pl-16 lg:basis-2/3">
-                      <CardItem
-                        cardImg={eve.img}
-                        cardContent={eve.content}
-                        cardTitlee={eve.title}
-                      />
-                    </CarouselItem>
-                 
-                  </div>
-              );
-            })}
-             </CarouselContent> 
+          <div className="cerodrag m-10">
+            <Carousel>
+              <CarouselContent>
+                {EventInfo.map((eve) => {
+                  return (
+                    <div key={eve.id}>
+                      <CarouselItem className="md:basis-1/2  pl-16 lg:basis-2/3">
+                        <CardItem
+                          cardImg={eve.img}
+                          cardContent={eve.content}
+                          cardTitlee={eve.title}
+                        />
+                      </CarouselItem>
+                    </div>
+                  );
+                })}
+              </CarouselContent>
             </Carousel>
           </div>
         </div>
--- src/Pages/Nav.jsx
+++ src/Pages/Nav.jsx
@@ -4,15 +4,31 @@ import React from "react";
 export default function Nav() {
   return (
     <div className="">
-  
-      <div className="flex justify-between text-center  border-b-2 navBg border-gray-200  bg-red-500 z-30 fixed top-0 w-full">
-        <span className="m-5 text-slate-200	text-xl cursor-pointer"><a href="#">IEEE GNITC</a></span>
-        <span className="m-5 text-slate-200	 cursor-pointer"><a href="#aboutus">About us</a></span>
-        <span className="m-5  text-slate-200	 cursor-pointer"><a href="#societies">Societies</a></span>
-        <span className="m-5 text-slate-200	 cursor-pointer"><a href="#events">Events</a></span>
-        <span className="m-5 text-slate-200	 cursor-pointer"><a href="#members">Members</a></span>
-        <span className="m-5 mr-10 text-slate-200	 cursor-pointer"><a href="#contact">Contact</a></span>
-
+      <div className="flex justify-between text-center  border-b-2 navBg border-gray-200   z-30 fixed top-0 w-full">
+        <div className="flex justify-between text-center    ">
+          <span className="m-5 text-slate-200	text-xl cursor-pointer">
+            <a href="#">IEEE GNITC</a>
+          </span>
+          <span className="m-5 text-slate-200	 cursor-pointer">
+            <a href="#aboutus">About us</a>
+          </span>
+          <span className="m-5  text-slate-200	 cursor-pointer">
+            <a href="#societies">Societies</a>
+          </span>
+          <span className="m-5 text-slate-200	 cursor-pointer">
+            <a href="#events">Events</a>
+          </span>
+          <span className="m-5 text-slate-200	 cursor-pointer">
+            <a href="#members">Members</a>
+          </span>
+          <span className="m-5 mr-10 text-slate-200	 cursor-pointer">
+            <a href="#contact">Contact</a>
+          </span>
+        </div>
+        <div className="flex justify-between text-center txt gap-14 mr-10 m-5">
+          <span className="cursor-pointer">links</span>
+          <span className="cursor-pointer">links</span>
+        </div>
       </div>
     </div>
   );
