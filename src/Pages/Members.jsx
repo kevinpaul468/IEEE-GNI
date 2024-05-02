10c837de714178f3f01f7fde42f83717639630e7
Singireddy Sumith reddy <sumithreddy.6080@gmail.com>
2024-05-02 12:28:59 +0530

    made changes

--- src/Pages/Members.jsx
+++ src/Pages/Members.jsx
@@ -1,33 +1,50 @@
-import MemItem from '@/components/Items/MemItem'
-import React from 'react'
-import MemInfo from '@/info/meminfo'
+import MemItem from "@/components/Items/MemItem";
+import React from "react";
+import MemInfo from "@/info/meminfo";
 
-
-
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 function Memebers() {
   return (
-   
-       <div id="members">
+    <div id="members">
       <div className="h-screen">
         <div className="   eventsbg border-b-8   h-screen w-full">
           <p>.</p>
           <div className=" flex justify-between items-center mt-6  gap-3">
             <div className=" heading ">Members</div>
-          
           </div>
           <div className=" memcard flex  gap-5 m-6">
-          {MemInfo.map((data)=>{
-            return(
-              <MemItem memimg={data.img}  memname={data.name} memrole={data.role}/>
-            )
-          })} 
+            <Carousel
+              opts={{
+                align: "start",
+                loop: true,
+              }}
+            >
+              <CarouselContent>
+                {MemInfo.map((data) => {
+                  return (
+                    <CarouselItem className="basis-1/4">
+                      <MemItem
+                        memimg={data.img}
+                        memname={data.name}
+                        memrole={data.role}
+                      />
+                    </CarouselItem>
+                  );
+                })}
+              </CarouselContent>
+            </Carousel>
           </div>
         </div>
       </div>
     </div>
-  
-  )
+  );
 }
 
-export default Memebers
+export default Memebers;
--- src/components/Items/MemItem.jsx
+++ src/components/Items/MemItem.jsx
@@ -6,6 +6,8 @@ function MemItem({ memimg,memname,memrole}) {
     <div>
   
     <div className="bg-slate-700 border-2 p-2  rounded-xl overflow-hidden shadow-md w-72">
+
+        
       <img src= {memimg} alt="Employee Image" className="w-72  hover:p-2  hover:rounded-2xl  h-auto rounded-xl" />
       <div className="p-4">
         <h2 className="text-xl font-semibold txt">{memname}</h2>
