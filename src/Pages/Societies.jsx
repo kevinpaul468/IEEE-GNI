749e079f4a7038f58a8ad12ff6223c6212062d2c
kevin <kevinpaul468@gmail.com>
2024-05-02 11:27:22 +0530

    added societys pics

--- src/Pages/Societies.jsx
+++ src/Pages/Societies.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-import computer_society from "../assets/computer_society.png";
-// import women_in_engineering from "../assets/women_in_engineering.png";
-import communication_society from "../assets/communication_society.png";
+import React from 'react'
+import computer_society from '../assets/computer_society.png'
+// import women_in_engineering from '../assets/women_in_engineering.png'
+import communication_society from '../assets/communication_society.png'
+import circuits_and_systems_society from '../assets/circuits_and_systems_society.png'
+import { Radius } from 'lucide-react'
+
 
 function Societies() {
   return (
@@ -17,9 +20,17 @@ function Societies() {
         <img src={women_in_engineering} alt='women in engineering'/>
         women in engineering</div> */}
 
+    <div> 
+        <img className='rounded-full max-w-80' src={circuits_and_systems_society} alt="circuits and systems society" />
+        comunication society
+        </div>
+
       <div> 
         <img src={communication_society} alt="communication society" />
-      </div>
+        comunication society
+        </div>
+
+      
     </div>
   );
 }
