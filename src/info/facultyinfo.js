da6fb1b09f27ca7835dc2d01959000731f7652d8
kevin <kevinpaul468@gmail.com>
2024-05-05 18:39:26 +0530

    made changes

--- src/info/facultyinfo.js
+++ src/info/facultyinfo.js
@@ -5,7 +5,7 @@ const FacultyInfo = [
       role: "INSTITUTE HEAD",
     },
     {
-        name: "Dr. Harish Kumar",
+        name: "Dr. Harish Kundra",
         img: "https://media.wired.com/photos/650399af65d83ff288720473/1:1/w_1285,h_1285,c_limit/If-Elon-Musk-Had-Been-a-Happy-Child,-Would-He-Still-Be-Launching-Rockets--Business-Redux-h_16082330.jpg",
         role: "Branch Counceller",
       },
