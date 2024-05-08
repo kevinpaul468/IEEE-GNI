e5f82550911e378d9e5a1cb82a6c9a6740168533
kevin <kevinpaul468@gmail.com>
2024-05-08 11:57:55 +0530

    made changes

--- src/Pages/FutureEventTimeLine.jsx
+++ src/Pages/FutureEventTimeLine.jsx
@@ -9,46 +9,53 @@ import circuits_and_systems_society from '../assets/circuits_and_systems_society
 
 function FutureEventTimeLine() {
   return (
-    <div>
-      <p>.</p>
+      <div id="events">
+        <div  >
+        <div className="h-screen">
+          <div className="   eventsbg border-b-4   w-full">
+            <p>.</p>
+            <div className=" flex justify-between items-center mt-12 gap-3">
+              <div className=" heading ">Events</div>
+            </div>
+            <VerticalTimeline>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+        date="25th May 2024"
+        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        icon={<img src={IEEE} alt="IEEE" className='rounded-full'/>}
+      >
+        <h3 className="vertical-timeline-element-title">IEEE GNITC Inauguration</h3>
+        <h5 className="vertical-timeline-element-subtitle">Lets kick start the IEEE GNITC student chapter</h5>
+        <p>
+          
+        </p>
+      </VerticalTimelineElement>
+    
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+        date="25th May 2024"
+        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        icon={<img src={computer_society} alt="computer society" className='rounded-full'/>}
+      >
+        <h3 className="vertical-timeline-element-title">National level online project competition</h3>
+        <h5 className="vertical-timeline-element-subtitle">build, code ,present</h5>
+        <p>
+          
+        </p>
+      </VerticalTimelineElement>
       
-
-...
-
-<VerticalTimeline>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-   
-    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-    date="25th May 2024"
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    icon={<img src={IEEE} alt="IEEE" className='rounded-full'/>}
-  >
-    <h3 className="vertical-timeline-element-title">IEEE GNITC Inauguration</h3>
-    <h5 className="vertical-timeline-element-subtitle">Lets kick start the IEEE GNITC student chapter</h5>
-    <p>
-      
-    </p>
-  </VerticalTimelineElement>
-
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-    date="25th May 2024"
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    icon={<img src={computer_society} alt="computer society" className='rounded-full'/>}
-  >
-    <h3 className="vertical-timeline-element-title">National level online project competition</h3>
-    <h5 className="vertical-timeline-element-subtitle">build, code ,present</h5>
-    <p>
-      
-    </p>
-  </VerticalTimelineElement>
-  
-</VerticalTimeline>
-    </div>
-  )
+    </VerticalTimeline>
+        </div>
+          </div>
+        </div>
+      </div>
+  );
 }
 
 export default FutureEventTimeLine
+
+
